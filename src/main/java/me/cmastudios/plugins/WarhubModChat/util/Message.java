5f80e02d3a0ebbfd0915e5abe0d81c7b034962d4
Connor M <cmastudios@charter.net>
2012-03-04 15:31:56 -0600

    Fix colorization issues (Test 4)

--- src/main/java/me/cmastudios/plugins/WarhubModChat/util/Message.java
+++ src/main/java/me/cmastudios/plugins/WarhubModChat/util/Message.java
@@ -19,22 +19,22 @@ public class Message {
 		System.out.println(message.replace("¤a", "").replace("¤b", "").replace("¤c", "").replace("¤d", "").replace("¤e", "").replace("¤f", "").replace("¤1", "").replace("¤2", "").replace("¤3", "").replace("¤4", "").replace("¤5", "").replace("¤6", "").replace("¤7", "").replace("¤8", "").replace("¤9", "").replace("¤0", ""));
 	}
 	public static String colorizeText(String string) {
-	     string.replace("&0", ChatColor.BLACK+"");
-	     string.replace("&1", ChatColor.DARK_BLUE+"");
-	     string.replace("&2", ChatColor.DARK_GREEN+"");
-	     string.replace("&3", ChatColor.DARK_AQUA+"");
-	     string.replace("&4", ChatColor.DARK_RED+"");
-	     string.replace("&5", ChatColor.DARK_PURPLE+"");
-	     string.replace("&6", ChatColor.GOLD+"");
-	     string.replace("&7", ChatColor.GRAY+"");
-	     string.replace("&8", ChatColor.DARK_GRAY+"");
-	     string.replace("&9", ChatColor.BLUE+"");
-	     string.replace("&a", ChatColor.GREEN+"");
-	     string.replace("&b", ChatColor.AQUA+"");
-	     string.replace("&c", ChatColor.RED+"");
-	     string.replace("&d", ChatColor.LIGHT_PURPLE+"");
-	     string.replace("&e", ChatColor.YELLOW+"");
-	     string.replace("&f", ChatColor.WHITE+"");
+	     string = string.replace("&0", ChatColor.BLACK+"");
+	     string = string.replace("&1", ChatColor.DARK_BLUE+"");
+	     string = string.replace("&2", ChatColor.DARK_GREEN+"");
+	     string = string.replace("&3", ChatColor.DARK_AQUA+"");
+	     string = string.replace("&4", ChatColor.DARK_RED+"");
+	     string = string.replace("&5", ChatColor.DARK_PURPLE+"");
+	     string = string.replace("&6", ChatColor.GOLD+"");
+	     string = string.replace("&7", ChatColor.GRAY+"");
+	     string = string.replace("&8", ChatColor.DARK_GRAY+"");
+	     string = string.replace("&9", ChatColor.BLUE+"");
+	     string = string.replace("&a", ChatColor.GREEN+"");
+	     string = string.replace("&b", ChatColor.AQUA+"");
+	     string = string.replace("&c", ChatColor.RED+"");
+	     string = string.replace("&d", ChatColor.LIGHT_PURPLE+"");
+	     string = string.replace("&e", ChatColor.YELLOW+"");
+	     string = string.replace("&f", ChatColor.WHITE+"");
 	    return string;
 	}
 	public String parseColor(String string) {
