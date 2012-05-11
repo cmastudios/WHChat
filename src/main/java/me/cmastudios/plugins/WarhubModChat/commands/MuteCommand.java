5e05d9d1fca47da5ca2c489dcf294452f089de45
cmastudios <cmastudios@charter.net>
2012-05-11 16:43:47 -0500

    Fixed mute problem and update version

--- src/main/java/me/cmastudios/plugins/WarhubModChat/commands/MuteCommand.java
+++ src/main/java/me/cmastudios/plugins/WarhubModChat/commands/MuteCommand.java
@@ -7,7 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class MuteCommand implements CommandExecutor {
 	public static WarhubModChat plugin;
@@ -18,49 +17,43 @@ public class MuteCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2,
 			String[] args) {
-		Player player = null;
-		if (sender instanceof Player) {
-			player=(Player)sender;
-		}
 		if (args.length < 1) {
-			player.sendMessage(ChatColor.YELLOW + "Muted players:");
+			// No args, display list
+			sender.sendMessage(ChatColor.YELLOW + "Muted players:");
 			String plrs = "";
 			for (String plr : plugin.mutedplrs.keySet()) {
 				plrs += plr + ", ";
 			}
-			player.sendMessage(ChatColor.YELLOW + plrs);
-			player.sendMessage(ChatColor.YELLOW
+			sender.sendMessage(ChatColor.YELLOW + plrs);
+			sender.sendMessage(ChatColor.YELLOW
 					+ "Use /mute <player> to mute someone.");
 			return true;
 		} else if (args.length == 1) {
-			Player todeafen = PlayerInfo.toPlayer(args[0]);
-			if (todeafen == player && player.hasPermission("warhub.moderator")) {
-				if (plugin.mutedplrs.containsKey(player.getName())) {
-					plugin.mutedplrs.remove(player.getName());
-					todeafen.sendMessage(ChatColor.YELLOW
+			// One argument, mute the player
+			String tomute;
+			if (PlayerInfo.isPlayer(args[0])) {
+				tomute = PlayerInfo.PlayerToString(PlayerInfo.toPlayer(args[0]));
+			} else {
+				sender.sendMessage(ChatColor.RED + args[0] + " is not online. Make sure name is exactly right");
+				tomute = args[0];
+			}
+			if (sender.hasPermission("warhub.moderator")) {
+				if (plugin.mutedplrs.containsKey(tomute)) {
+					plugin.mutedplrs.remove(tomute);
+					sender.sendMessage(ChatColor.YELLOW
+							+ tomute + " has been unmuted.");
+					if (PlayerInfo.isPlayer(tomute)) PlayerInfo.toPlayer(tomute).sendMessage(ChatColor.YELLOW
 							+ "You have been unmuted.");
 				} else {
-					plugin.mutedplrs.put(player.getName(), 1);
-					todeafen.sendMessage(ChatColor.YELLOW
-							+ "You have been muted.");
-				}
-			} else if (player.hasPermission("warhub.moderator")) {
-				if (plugin.mutedplrs.containsKey(todeafen.getName())) {
-					plugin.mutedplrs.remove(todeafen.getName());
-					player.sendMessage(ChatColor.YELLOW
-							+ todeafen.getName() + " has been unmuted.");
-					todeafen.sendMessage(ChatColor.YELLOW
-							+ "You have been unmuted.");
-				} else {
-					plugin.mutedplrs.put(todeafen.getName(), 1);
-					player.sendMessage(ChatColor.YELLOW
-							+ todeafen.getName() + " has been muted.");
-					todeafen.sendMessage(ChatColor.YELLOW
+					plugin.mutedplrs.put(tomute, 1);
+					sender.sendMessage(ChatColor.YELLOW
+							+ tomute + " has been muted.");
+					if (PlayerInfo.isPlayer(tomute)) PlayerInfo.toPlayer(tomute).sendMessage(ChatColor.YELLOW
 							+ "You have been muted.");
 				}
 
 			} else {
-				player.sendMessage(ChatColor.RED
+				sender.sendMessage(ChatColor.RED
 						+ "You do not have permissions to mute players.");
 			}
 			return true;
